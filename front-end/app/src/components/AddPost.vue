f5d57d7982848393091523dcf3552cbc6d0deac6
mavosk <Trever1996>
2019-02-17 19:02:03 +0200

    esialgne disain

--- front-end/app/src/components/AddPost.vue
+++ front-end/app/src/components/AddPost.vue
@@ -67,5 +67,41 @@ export default {
 </script>
 
 <style scoped>
+  label {
+    text-align: right;
+  }
+  form {
+    display: compact;
+  }
+  input {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type=text] {
+    width: 30%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+
+  input[type=submit] {
+    width: 30%;
+    background-color: #333;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  div {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+  }
 
 </style>
