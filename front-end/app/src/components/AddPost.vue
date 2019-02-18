b62166cedbf81784123bd558dce16f7c21c947fa
alvalt <alvalt@ttu.ee>
2019-02-19 00:48:11 +0200

    Post validation

--- front-end/app/src/components/AddPost.vue
+++ front-end/app/src/components/AddPost.vue
@@ -4,15 +4,15 @@
       <form id="post-form" @submit.prevent="processForm">
         Title:<br>
         <input type="text" name="title" placeholder="Title" v-model="title"  v-validate="{ required: true, min: 3 }"><br>
-        <div class="error" v-if=" submitted && errors.has('title')">{{errors.first('title')}}</div>
+        <div class="error" v-if="errors.has('title')">{{errors.first('title')}}</div>
         Description:<br>
-        <input type="text" name="description" placeholder="Description" v-model="description" v-validate="{ required: true, min: 3 }"><br>
-        <div class="error" v-if=" submitted && errors.has('description')">{{errors.first('description')}}</div>
+        <input type="text" name="description" placeholder="Description" v-model="description" v-validate="{ required: true, min: 10 }"><br>
+        <div class="error" v-if="errors.has('description')">{{errors.first('description')}}</div>
         Reward description:<br>
         <input type="text" name="reward_description" placeholder="Reward Description"
                v-model="reward_description" v-validate="{ required: true, min: 10
                 }"><br>
-        <div class="error" v-if="submitted && errors.has('reward_description')">{{errors.first('reward_description')}}</div>
+        <div class="error" v-if="errors.has('reward_description')">{{errors.first('reward_description')}}</div>
         File:<br>
         <input id="singleFileUploadInput" type="file" name="file" class="file-input"
                @change="loadTextFromFile"/><br>
@@ -34,7 +34,7 @@ export default {
       reward_description: '',
       file: null,
       return_msg: '',
-      submitted: false
+
 
 
     };
@@ -55,59 +55,57 @@ export default {
 
 
     processForm() {
-      this.submitted = true;
+
       this.$validator.validate().then(valid => {
         if (valid) {
           alert("Successful!");
+          if (this.title === '') {
+            return;
+          }
+
+          if (this.file === null) {
+            axios
+                    .post('http://localhost:8090/api/addpost', {
+                      title: this.title,
+                      description: this.description,
+                      rewardDescription: this.reward_description })
+                    .then((response) => {
+                      if (response.status === 200) {
+                        this.return_msg = "Post successfully uploaded!";
+                        this.emptyFields();
+                      } else {
+                        this.return_msg = "Sorry, there was a problem uploading Your post!";
+                      }
+                    });
+            // WITH FILE
+          } else {
+            const formData = new FormData();
+            formData.append('file', this.file);
+            axios
+                    .post('http://localhost:8090/api/uploadFile', formData)
+                    .then((response) => {
+                      if (response.status === 200) {
+                        axios
+                                .post('http://localhost:8090/api/addpost', {
+                                  title: this.title,
+                                  description: this.description,
+                                  rewardDescription: this.reward_description,
+                                  fileLocation: response.data.fileDownloadUri })
+                                .then((response) => {
+                                  if (response.status === 200) {
+                                    this.return_msg = "Post successfully uploaded!";
+                                    this.emptyFields();
+                                  } else {
+                                    this.return_msg = "Sorry, there was a problem uploading Your post!";
+                                  }
+                                });
+                      }
+                    });
+          }
         } else {
           alert("Fail to submit Form!");
         }
       });
-
-      if (this.title === '') {
-        return;
-      }
-
-      // IF NO FILE
-      if (this.file === null) {
-        axios
-          .post('http://localhost:8090/api/addpost', {
-            title: this.title,
-            description: this.description,
-            rewardDescription: this.reward_description })
-          .then((response) => {
-            if (response.status === 200) {
-              this.return_msg = "Post successfully uploaded!";
-              this.emptyFields();
-            } else {
-              this.return_msg = "Sorry, there was a problem uploading Your post!";
-            }
-          });
-      // WITH FILE
-      } else {
-        const formData = new FormData();
-        formData.append('file', this.file);
-        axios
-          .post('http://localhost:8090/api/uploadFile', formData)
-          .then((response) => {
-            if (response.status === 200) {
-              axios
-                .post('http://localhost:8090/api/addpost', {
-                  title: this.title,
-                  description: this.description,
-                  rewardDescription: this.reward_description,
-                  fileLocation: response.data.fileDownloadUri })
-                  .then((response) => {
-                    if (response.status === 200) {
-                        this.return_msg = "Post successfully uploaded!";
-                        this.emptyFields();
-                    } else {
-                        this.return_msg = "Sorry, there was a problem uploading Your post!";
-                    }
-              });
-            }
-          });
-      }
     },
   },
 };
