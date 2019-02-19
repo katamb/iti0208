f352bab09516e212881340d6f835ed2eca95a77a
mavosk <Trever1996>
2019-02-19 16:05:52 +0200

    esilehe esialgne disain

--- front-end/app/src/components/ViewPosts.vue
+++ front-end/app/src/components/ViewPosts.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>Sample data</h2>
+    <h2>Last uploaded</h2>
     <div class="items" v-for="(data, index) in response" :key='index'>
       <h3>{{data.title}}</h3>
       <p>{{data.description}}</p>
@@ -32,7 +32,30 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .items {
-    background-color: #a3a3a3;
+  h3 {
+    color: #333;
   }
+  .items {
+    width: 80%;
+    margin: 0px auto;
+    background-color: lightgray;
+    color: black;
+    text-align: center;
+    border:1px solid #333;
+    border-radius: 4px;
+  }
+
+  .items:hover {
+    background-color: #fff;
+    cursor: pointer;
+  }
+  .items:active {
+    background-color: #F1F1F1;
+    cursor: pointer;
+  }
+
+  body {
+    background-color: lightgray;
+  }
+
 </style>
