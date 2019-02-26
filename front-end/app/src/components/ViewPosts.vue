671a13737a3760de028b425ac47f6af109237014
mavosk <Trever1996>
2019-02-26 12:10:30 +0200

    tegin ühe postituse vaate

--- front-end/app/src/components/ViewPosts.vue
+++ front-end/app/src/components/ViewPosts.vue
@@ -1,25 +1,28 @@
 <template>
     <div>
-        <h2 v-if="topic === 'all'" >Last uploaded</h2>
+        <h2 v-if="topic === 'all'">Last uploaded</h2>
         <div class="items" v-for="data in response" :key='data.id'>
 
             <router-link :to="{path : '/viewpost/'+ data.id }" push>
-            <h3>{{data.title}}</h3>
-            <p>{{data.description}}</p>
+                <h3>{{data.title}}</h3>
+                <p>{{data.description}}</p>
             </router-link>
+
             <!--p>{{data.rewardDescription}}</p>
             <a v-if="data.fileLocation" v-bind:href=data.fileLocation>Extra information</a-->
         </div>
-
+        <br>
         <button v-if="currentPageNum > 0" type="button"
-                v-on:click="prevPage()">Previous!</button>
+                v-on:click="prevPage()">Previous!
+        </button>
         <button v-if="currentPageNum < numOfPages" type="button"
-                v-on:click="nextPage()">Next!</button>
+                v-on:click="nextPage()">Next!
+        </button>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+    import axios from 'axios';
 
     export default {
         name: 'ViewPosts',
@@ -85,27 +88,42 @@ import axios from 'axios';
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    color: #333;
-  }
-  .items {
-    width: 80%;
-    margin: 0px auto;
-    background-color: lightgray;
-    color: black;
-    text-align: center;
-    border:1px solid #333;
-    border-radius: 4px;
-  }
+    h3 {
+        color: #333;
+    }
 
-  .items:hover {
-    background-color: #fff;
-    cursor: pointer;
-  }
-  .items:active {
-    background-color: #F1F1F1;
-    cursor: pointer;
-  }
+    .items {
+        width: 80%;
+        margin: 0px auto;
+        background-color: lightgray;
+        color: black;
+        text-align: center;
+        border: 1px solid #333;
+        border-radius: 4px;
+    }
+
+    .items:hover {
+        background-color: #fff;
+        cursor: pointer;
+    }
+
+    .items:active {
+        background-color: #F1F1F1;
+        cursor: pointer;
+    }
+
+    button[type="button"] {
+        display: inline-block;
+        width: 100px;
+        height: 50px;
+        border: 1px solid #333;
+        border-radius: 4px;
+        background-color: #333;
+        color: white;
+        cursor: pointer;
+
+    }
+
 
     body {
         background-color: lightgray;
