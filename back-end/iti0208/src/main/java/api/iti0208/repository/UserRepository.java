77a9cb6726b9b45333ca37e0d25c7745010c4342
katamb <katamb@ttu.ee>
2019-04-20 12:19:45 +0300

    backend auto deploy, security JPA fix, varchar(255) to text

--- back-end/iti0208/src/main/java/api/iti0208/repository/UserRepository.java
+++ back-end/iti0208/src/main/java/api/iti0208/repository/UserRepository.java
@@ -5,7 +5,9 @@ import api.iti0208.data.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource(exported = false)
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
     AppUser findByUsername(String username);
